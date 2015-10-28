3cdc6d870689fa7851d372c33383e60ce4bb2bb6
edwardiscsu <ised03001@stcloudstate.edu>
2015-10-28 00:57:38 -0500

    Added CORS handler for auth login (/auth/login).

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,14 @@
 var express = require('express');
 var router = express.Router();
 
+/* CORS handler. IMPORTANT! Make sure it's there for all your routes! */
+router.all('/login', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+    next();
+});
+
 /* POST a login request. */
 router.post('/login', function(req, res, next) {
     var email = req.body.email;
