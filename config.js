115b679c91d642e4cb4952a0daefc55bfd89ef8f
edwardiscsu <ised03001@stcloudstate.edu>
2015-11-07 02:23:34 -0600

    Finished initial mongodb connection

--- config.js
+++ config.js
@@ -3,7 +3,11 @@
  */
 
 var config = {
-    dbUrl : 'mongodb://roomiecon-api:roomiecon@ds045734.mongolab.com:45734/scsu-csci550-roomiecon'
+    dbUri : 'mongodb://roomiecon-api:roomiecon@ds045734.mongolab.com:45734/scsu-csci550-roomiecon',
+    dbOptions : {
+        server:  { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+        replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+    }
 }
 
 module.exports = config;
--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,9 @@ var router = express.Router();
 
 var config = require('../config');
 var mongoose = require('mongoose');
-mongoose.connect(config.dbUrl);
+var uriUtil = require('mongodb-uri');
+var mongooseUri = uriUtil.formatMongoose(config.dbUri);
+
 
 /* CORS handler. IMPORTANT! Make sure it's there for all your routes! */
 router.all('/login', function(req, res, next) {
@@ -17,33 +19,30 @@ router.all('/login', function(req, res, next) {
 
 /* POST a login request. */
 router.post('/login', function(req, res, next) {
-    var test = config.dbUrl;
     var email = req.body.email;
     var password = req.body.password;
 
+    mongoose.connect(mongooseUri, config.dbOptions);
     var db = mongoose.connection;
-    //db.on('error', console.error.bind(console, 'connection error'));
     db.on('error', function() {
-        var test = 'hello';
+        console.error.bind(console, 'POST /login db connection error');
+        res.status(500).send({
+            error: 'error connecting to database'
+        })
     });
     db.once('open', function(callback) {
-        res.status(200).send({
-            status: true,
-            email: email
+        res.setHeader('Content-Type', 'application/json');
+        res.format({
+            'application/json' : function() {
+                res.status(200).send({
+                    status: true,
+                    email: email
+                });
+            },
+            'default' : function() {
+                res.status(406).send('Content-Type Required');
+            }
         });
-    });
-
-    es.setHeader('Content-Type', 'application/json');
-    res.format({
-        'application/json' : function() {
-            res.status(200).send({
-                status: true,
-                email: email
-            });
-        },
-        'default' : function() {
-            res.status(406).send('Content-Type Required');
-        }
     });
 });
 
@@ -54,7 +53,7 @@ router.all('/signup', function(req, res, next) {
     next();
 });
 
-/* POST a login request. */
+/* POST a signup request. */
 router.post('/signup', function(req, res, next) {
     var email = req.body.email;
     var password = req.body.password;
