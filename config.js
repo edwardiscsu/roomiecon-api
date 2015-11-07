a6d8d2148f6d35ad87a5a73b80186efb7e9a4c48
edwardiscsu <ised03001@stcloudstate.edu>
2015-11-07 01:53:21 -0600

    Added initial mongoose connection test code

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Edward on 11/7/2015.
+ */
+
+var config = {
+    dbUrl : 'mongodb://roomiecon-api:roomiecon@ds045734.mongolab.com:45734/scsu-csci550-roomiecon'
+}
+
+module.exports = config;
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,10 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('../config');
+var mongoose = require('mongoose');
+mongoose.connect(config.dbUrl);
+
 /* CORS handler. IMPORTANT! Make sure it's there for all your routes! */
 router.all('/login', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -13,10 +17,23 @@ router.all('/login', function(req, res, next) {
 
 /* POST a login request. */
 router.post('/login', function(req, res, next) {
+    var test = config.dbUrl;
     var email = req.body.email;
     var password = req.body.password;
 
-    res.setHeader('Content-Type', 'application/json');
+    var db = mongoose.connection;
+    //db.on('error', console.error.bind(console, 'connection error'));
+    db.on('error', function() {
+        var test = 'hello';
+    });
+    db.once('open', function(callback) {
+        res.status(200).send({
+            status: true,
+            email: email
+        });
+    });
+
+    es.setHeader('Content-Type', 'application/json');
     res.format({
         'application/json' : function() {
             res.status(200).send({
