347a3ea2469f7d3e64405d4a942f80768bc153d1
MunaShrestha520 <shmu1201@stcloudstate.edu>
2015-11-06 19:21:04 -0600

    SignUp API

--- routes/auth.js
+++ routes/auth.js
@@ -30,4 +30,30 @@ router.post('/login', function(req, res, next) {
     });
 });
 
+router.all('/signup', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+    next();
+});
+
+/* POST a login request. */
+router.post('/signup', function(req, res, next) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    res.setHeader('Content-Type', 'application/json');
+    res.format({
+        'application/json' : function() {
+            res.status(200).send({
+                status: true,
+                email: email
+            });
+        },
+        'default' : function() {
+            res.status(406).send('Content-Type Required');
+        }
+    });
+});
+
 module.exports = router;
